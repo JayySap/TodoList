583b625eb0a62fd36bd052df3e6d4e3ef7fa3c1c
Jayant Sapra <saprajayant@gmail.com>
2022-07-04 10:55:58 -0700

    todo list added

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+//Importing Components
+import Form from './components/Form';
+import ToDoList from './components/ToDoList';
+
+function App() {
+
+  // State stuff
+  const [inputText, setInputText] = useState("");
+  const [todos , setTodos] = useState([]);
+  const [status, setStatus] = useState('all');
+  const [filteredTodos, setFilteredTodos] = useState([]);
+
+  // Use effect
+  useEffect(() => {
+    filterHandler();
+  }, [todos, status]);
+  // Functions
+  const filterHandler = () => {
+    switch(status){
+      case 'completed':
+        setFilteredTodos(todos.filter(todo => todo.completed === true));
+          break;
+        case 'uncompleted':
+          setFilteredTodos(todos.filter(todo => todo.completed === false));
+          break;
+        default:
+          setFilteredTodos(todos);
+          break;
+    }
+  }
+  return (
+    <div className="App">
+      <header>
+        <h1>Jayant's ToDo List </h1>
+      </header>
+      <Form 
+        inputText = {inputText}
+        todos={todos} 
+        setTodos = {setTodos} 
+        setInputText = {setInputText}
+        setStatus = {setStatus}
+        
+        />
+      <ToDoList 
+        filteredTodos={filteredTodos} 
+        setTodos={setTodos} 
+        todos = {todos} 
+        />
+    </div>
+  );
+}
+
+export default App;
